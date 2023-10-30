39baf5f4943945aea16c5477aceba0fd74bdd1b0
patsenky <149412077+patsenky@users.noreply.github.com>
2023-10-30 12:31:54 -0400

    Update RGB_LED2.ino

--- RGB_LED2/RGB_LED2.ino
+++ RGB_LED2/RGB_LED2.ino
@@ -8,12 +8,12 @@
 
 void setup()
 {
-pinMode(RED, OUTPUT);
-pinMode(GREEN, OUTPUT);
-pinMode(BLUE, OUTPUT);
-digitalWrite(RED, HIGH);
-digitalWrite(GREEN, HIGH);
-digitalWrite(BLUE, LOW);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, LOW);
 }
 
 // define variables
@@ -21,33 +21,36 @@ int redValue;
 int greenValue;
 int blueValue;
 
-
 // main loop
 void loop()
 {
-#define delayTime 30
+  // Define a delay time
+  #define delayTime 30
 
-int redValue = 255;
-int greenValue = 40;
-int blueValue = 0;
+  // Initialize color values
+  int redValue = 255;
+  int greenValue = 40;
+  int blueValue = 0;
 
-for(int i = 0; i < 40; i += 1) 
-{
-greenValue += 1;
-analogWrite (RED, redValue);
-analogWrite(GREEN, greenValue);
-delay(delayTime);
+  // Increase green color to make orangeish yellow
+  for(int i = 0; i < 40; i += 1) 
+  {
+    greenValue += 1;
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+    delay(delayTime);
+  }
+
+  // Reset color values
+  redValue = 255;
+  greenValue = 80;
+  blueValue = 0;
+
+  // Decrease green color, back to red
+  for(int i = 0; i < 40; i += 1)
+  {
+    greenValue -= 1;
+    analogWrite(GREEN, greenValue);
+    delay(delayTime);
+  }
 }
-
-redValue = 255;
-greenValue = 80;
-blueValue = 0;
-
-for(int i = 0; i < 40; i += 1)
-{
-greenValue -= 1;
-analogWrite(GREEN, greenValue);
-delay(delayTime);
-}}
-
-
